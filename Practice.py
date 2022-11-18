4a4349323c797545310128b9f90294b4be20c6d5
Charles Franklin <ctfranklin28@gmail.com>
2022-11-17 23:38:02 -0500

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,14 @@
+Python 3.9.2 (default, Feb 28 2021, 17:03:44) 
+[GCC 10.2.1 20210110] on linux
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 'Alice' + 'Bob'
+'AliceBob'
+>>> 'Alice' * 5
+'AliceAliceAliceAliceAlice'
+>>> spam = 40
+>>> spam
+40
+>>> eggs = 2
+>>> spam + eggs
+42
+>>> 
